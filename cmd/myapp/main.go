6a609b0cd2e20ac7a954eb845bccfb02e6bab3be
mattangoh <mattangoh@gmail.com>
2024-08-10 17:56:46 -0700

    added user error checking
    Signed-off-by: mattangoh <mattangoh@gmail.com>

--- cmd/myapp/main.go
+++ cmd/myapp/main.go
@@ -8,13 +8,33 @@ import (
 func main() {
   
   var x,y float64
+  
+  for {
 
-  fmt.Print("Please enter the value of the first side: ")
-  fmt.Scan(&x)
+    fmt.Print("Please enter the value of the first side: ")
+    _, err := fmt.Scan(&x)
 
-  fmt.Print("Please enter the value of the second side: ")
-  fmt.Scan(&y)
+    if err != nil {
+      fmt.Println("Error... Please enter a valid float")
+      var discard string
+      fmt.Scanln(&discard)
+    } else {
+        break
+    }
+  }
+  for {
+      fmt.Print("Please enter the value of the second side: ")
+      _, err := fmt.Scan(&y)
 
+      if err != nil {
+        fmt.Println("Error... Please enter a valid float")
+        var discard string
+        fmt.Scanln(&discard)
+      } else {
+          break
+      }
+  }    
+  
   hyp := calculateHypotenuse(x, y)
 
   fmt.Printf("The hypotenuse is %v!", hyp)
