8a613df2fb103f26f56408c77a755cf50a78d028
mattangoh <mattangoh@gmail.com>
2024-08-10 17:43:41 -0700

    added user input
    Signed-off-by: mattangoh <mattangoh@gmail.com>

--- cmd/myapp/main.go
+++ cmd/myapp/main.go
@@ -7,12 +7,17 @@ import (
 
 func main() {
   
-  x := 3.0
-  y := 4.0
+  var x,y float64
+
+  fmt.Print("Please enter the value of the first side: ")
+  fmt.Scan(&x)
+
+  fmt.Print("Please enter the value of the second side: ")
+  fmt.Scan(&y)
 
   hyp := calculateHypotenuse(x, y)
 
-  fmt.Printf("The hypotenuse is %v", hyp)
+  fmt.Printf("The hypotenuse is %v!", hyp)
 
 }
 
